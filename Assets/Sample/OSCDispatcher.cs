2dafcb55cda8725f1154c809593df3deff7c9ced
Manuel Aráoz <manuelaraoz@gmail.com>
2014-06-14 16:06:29 -0300

    first commit

---
+++ Assets/Sample/OSCDispatcher.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using OSC;
+
+public class OSCDispatcher : MonoBehaviour {
+	
+	[SerializeField]
+	int port = 7000;
+	
+	[SerializeField]
+	string hostname = "localhost";
+	
+	OSCManager oscManager;
+
+	void OnEnable() {
+		oscManager = OSCManager.instance;
+		oscManager.LocalPort = port;
+	}
+	
+	void OnDisable() {
+		
+	}
+
+    public void AddListener(OSCManager.OscMessageDelegate listener){
+        oscManager.OnMessage += listener;
+    }
+
+    
+}
---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using OSC;
+
+public class Cube : MonoBehaviour {
+
+    public OSCDispatcher dispatcher;
+    private Vector3 p0;
+
+	// Use this for initialization
+	void Start () {
+        dispatcher.AddListener(OnMessage);
+        p0 = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMessage(OSCMessage message)
+    {
+        Debug.Log(message.Address);
+        ArrayList values = message.Values;
+        for (int i = 0; i < values.Count; i++) {
+            Debug.Log(values[i]);
+        }
+
+        float x = (float) values[0];
+        float y = (float) values[1];
+        Vector3 pos = transform.position;
+        pos.x = p0.x + x;
+        pos.z = p0.z + y;
+        transform.position = pos;
+
+    }
+}
